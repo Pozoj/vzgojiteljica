4e8da32be4ab0bddbf794f72af8d06940a1cea70
Patricija Valentinuzzi <patricija.valentinuzzi@gmail.com>
2013-04-04 12:11:18 -0700

    Defining CRUD links on Inquires page. (inquires_controller)

--- app/controllers/inquiries_controller.rb
+++ app/controllers/inquiries_controller.rb
@@ -1,6 +1,19 @@
 class InquiriesController < InheritedResources::Base
   skip_before_filter :authenticate, only: [:new, :create]
 
+  def destroy
+    destroy! { all_inquiries_path }
+  end
+
+
+  def create
+  	create! { inquiries_path }
+  end
+
+  def update
+  	update! { inquiries_path }
+  end
+
   private
 
     def resource_params

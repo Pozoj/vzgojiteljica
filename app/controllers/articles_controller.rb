97bb56d63baf24308659d6c74c8e011e56b72783
Patricija Valentinuzzi <patricija.valentinuzzi@gmail.com>
2013-04-04 12:10:43 -0700

    Def collection (Articles Controller)

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,6 +1,10 @@
 class ArticlesController < InheritedResources::Base
   private
 
+  def collection
+  	end_of_association_chain.limit(30)
+  end
+
   def resource_params
     return [] if request.get?
     [params.require(:article).permit(:section_id, :issue_id, :title, :abstract_html, :abstract_english_html)]

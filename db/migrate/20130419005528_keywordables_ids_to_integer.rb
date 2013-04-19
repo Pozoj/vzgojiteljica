9dfe508713adce944c2fae00eb0c9448f968b03b
Miha Rebernik <miha@rebernik.info>
2013-04-18 21:28:02 -0700

    Adding special migration instructions for production PG.

--- db/migrate/20130419005528_keywordables_ids_to_integer.rb
+++ db/migrate/20130419005528_keywordables_ids_to_integer.rb
@@ -1,6 +1,11 @@
 class KeywordablesIdsToInteger < ActiveRecord::Migration
   def change
-    change_column :keywordables, :article_id, :integer
-    change_column :keywordables, :keyword_id, :integer
+    if Rails.env.development?
+      change_column :keywordables, :article_id, :integer
+      change_column :keywordables, :keyword_id, :integer
+    elsif Rails.env.production?
+      execute %{ALTER TABLE "keywordables" ALTER COLUMN article_id TYPE integer USING CAST(CASE article_id WHEN '' THEN NULL ELSE article_id END AS INTEGER)}
+      execute %{ALTER TABLE "keywordables" ALTER COLUMN keyword_id TYPE integer USING CAST(CASE keyword_id WHEN '' THEN NULL ELSE keyword_id END AS INTEGER)}
+    end
   end
 end

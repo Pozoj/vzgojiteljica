e39fd2739c201026eccaeb14adbfbdde36eea36b
Miha Rebernik <miha@rebernik.info>
2015-02-28 23:01:48 -0800

    Destroying subscriptions if we destroy the subscriber.

--- app/models/subscriber.rb
+++ app/models/subscriber.rb
@@ -1,6 +1,6 @@
 class Subscriber < Entity
   belongs_to :customer
-  has_many :subscriptions
+  has_many :subscriptions, dependent: :destroy
   has_one :contact_person, foreign_key: :entity_id
   has_one :billing_person, foreign_key: :entity_id
 

92af4f4f70bd9ad56da773eea7ff659ab9022c3e
Miha Rebernik <miha@rebernik.info>
2015-03-01 18:29:33 -0800

    Optimizing regional.

--- app/controllers/admin/admin_controller.rb
+++ app/controllers/admin/admin_controller.rb
@@ -32,12 +32,30 @@ class Admin::AdminController < ApplicationController
   end
 
   def regional
-    customers = Customer.all.reject do |customer|
+    customers = Customer.all.order(:post_id).reject do |customer|
       subscriptions = customer.subscriptions.active.empty?
     end
 
-    @regional = customers.group_by { |customer| customer.post.try(:master) }
-    .reject { |region, customers| region.nil? }
-    @total = customers.count
+    @total_customers = 0
+    @total_quantity = 0
+
+    @regional = customers.group_by do |customer| 
+      customer.post.try(:master)
+    end.reject do |region, customers| 
+      region.nil?
+    end.map do |region, customers|
+      customers_count = customers.count
+      @total_customers += customers_count
+      quantities_count = customers.inject(0) { |sum, customer| sum += customer.subscriptions.active.sum(:quantity) }
+      @total_quantity += quantities_count
+
+      [region, {
+        customers_count: customers_count,
+        quantities_count: quantities_count
+      }]
+    end
+
+    @total_customers = customers.count
+    @total_quantity = Subscription.active.sum(:quantity)
   end
 end

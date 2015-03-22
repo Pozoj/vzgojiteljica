c4d2f4a3ef0cd4dbdaf82e37e43256b2054256b2
Miha Rebernik <miha@rebernik.info>
2015-03-21 21:57:05 -0700

    Adding bank seeding migration.

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150309031905) do
+ActiveRecord::Schema.define(version: 20150322045608) do
 
   create_table "articles", force: :cascade do |t|
     t.integer  "section_id"

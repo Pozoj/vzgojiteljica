7edf2c597cf7ce17656dccff32427cc49cb3c701
Miha Rebernik <miha@rebernik.info>
2015-05-31 13:09:54 -0700

    Fix to include regular people.

--- config/initializers/cmxl.rb
+++ config/initializers/cmxl.rb
@@ -2,7 +2,7 @@ Cmxl.config[:raise_line_format_errors] = false
 
 class MyFieldParser < Cmxl::Field
   self.tag = 86 # define which MT940 tag your parser can handle. This will automatically register your parser and overwriting existing parsers
-  self.parser = /\/SIO\/(?<transaction_codes>.*)\/SIB\/(?<reference>.*)\/ACC\/(?<iban>.*)\/PAR\/(?<entity>.*)SEPA/ # the regex to parse the line. Use named regexp to access your match.
+  self.parser = /\/(SIO|ROC)\/(?<transaction_codes>.*)\/SIB\/(?<reference>.*)\/ACC\/(?<iban>.*)\/PAR\/(?<entity>.*)SEPA/ # the regex to parse the line. Use named regexp to access your match.
 
   def iban
     self.data['iban']

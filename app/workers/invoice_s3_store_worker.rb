a5bf55d232869d56ab6a48304a1a608fcf17b822
Miha Rebernik <miha@rebernik.info>
2015-06-01 08:35:45 -0700

    Don’t store e-invoices for folks who don’t support them.

--- app/workers/invoice_s3_store_worker.rb
+++ app/workers/invoice_s3_store_worker.rb
@@ -3,6 +3,7 @@ class InvoiceS3StoreWorker
   def perform(invoice_id)
     return unless invoice = Invoice.find(invoice_id)
     invoice.store_pdf
+    return unless invoice.customer.einvoice?
     invoice.store_einvoice
     invoice.store_eenvelope
   end

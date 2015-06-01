b7a122f9a048536fce1c0a1cbff62ded4cdea206
Miha Rebernik <miha@rebernik.info>
2015-06-01 08:51:46 -0700

    Only retry upload once.

--- app/workers/invoice_s3_store_worker.rb
+++ app/workers/invoice_s3_store_worker.rb
@@ -1,5 +1,7 @@
 class InvoiceS3StoreWorker
   include Sidekiq::Worker
+  sidekiq_options :retry => 1
+
   def perform(invoice_id)
     return unless invoice = Invoice.find(invoice_id)
     invoice.store_pdf

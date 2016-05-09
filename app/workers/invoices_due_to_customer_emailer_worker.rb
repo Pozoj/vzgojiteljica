afe60a3cf3e2d6d7c8387c10f132525b3d8c75e8
Miha Rebernik <miha@rebernik.info>
2016-05-08 22:18:13 -0700

    Adding automatic invoice due emaile worker

---
+++ app/workers/invoices_due_to_customer_emailer_worker.rb
@@ -0,0 +1,26 @@
+class InvoicesDueToCustomerEmailerWorker
+  include Sidekiq::Worker
+  sidekiq_options :retry => false
+
+  def perform
+    return if Date.today.saturday? || Date.today.sunday?
+
+    Invoice.unpaid.due.where(year: Date.today.year).order(receipt_id: :asc).each do |invoice|
+      # Skip if invoice is not yet due for a week.
+      next if invoice.due_at > 1.week.ago
+
+      last_due_email_sent = invoice.events.where(event: :invoice_due_sent).order(created_at: :desc).first
+
+      # Skip if invoice due was emailed in the past week.
+      next if last_due_email_sent && last_due_email_sent.created_at > 1.week.ago
+
+      # Check the user has a billing email set.
+      next unless invoice.customer.billing_email.present?
+
+      # Now send the due email.
+      Mailer.delay.invoice_due_to_customer(invoice.id)
+
+      puts "Sending due email to #{invoice.customer}"
+    end
+  end
+end

4b894f0178a1536ea38832eb9e0e260f0f4c5596
Miha Rebernik <miha@rebernik.info>
2016-11-22 20:48:21 -0800

    Email receipts upon creation if customer not an einvoice customer

--- app/workers/receipt_wizard_worker.rb
+++ app/workers/receipt_wizard_worker.rb
@@ -10,7 +10,23 @@ class ReceiptWizardWorker
       receipts = @receipt_wizard.create_per_issue_receipts
     end
 
+    receipts = receipts.compact.uniq
+
     # Queue S3 uploads.
-    ReceiptsS3StoreWorker.perform_async(receipts.compact.uniq.map(&:id))
+    ReceiptsS3StoreWorker.perform_async(receipts.map(&:id))
+
+    # Queue emails.
+    receipts.each do |receipt|
+      customer = receipt.customer
+
+      next if customer.einvoice?
+
+      unless customer.billing_email.present?
+        Scrolls.log(message: "No customer billing email present!")
+        next
+      end
+
+      Mailer.delay.invoice_to_customer(receipt.id)
+    end
   end
 end
